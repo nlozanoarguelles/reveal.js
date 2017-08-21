d1b4c8a645fa9c540f5e3970f43f132305871065
Nicolás Lozano <nlozano@divisadero.es>
2017-08-21 12:38:36 +0200

    updating host

--- plugin/multiplex/client.js
+++ plugin/multiplex/client.js
@@ -6,7 +6,7 @@
 	socket.on(multiplex.id, function(data) {
 		// ignore data from sockets that aren't ours
 		if (data.socketId !== socketId) { return; }
-		if( window.location.host === 'localhost:1947' ) return;
+		if( window.location.host === 'barometroquiz.divisadero.es:1947' ) return;
 
 		Reveal.setState(data.state);
 	});
